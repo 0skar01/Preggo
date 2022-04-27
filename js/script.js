843769d00e977bb625714ef617618c9e7994c574
0skar01 <100316643+0skar01@users.noreply.github.com>
2022-04-27 14:53:25 +0200

    register.html JQuery

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-$("#radio1").on("click", function(){
-    $("#slider").show();
+$("#radio1").on("click", function(e){
+    $("#sliderDiv").show();
 });
-$("#radio2").on("click", function(){
-    $("#slider").hide();
+$("#radio2").on("click", function(e){
+    $("#sliderDiv").hide();
+});
+$("#slider").on("input", function(e){
+    $("#output").html($(this).val() + " weeks");
 });
